db6609e2dd43aec4014577eb3df1e02ea29e9b54
Jas0ncn <ceocjy@vip.qq.com>
2016-12-27 19:21:05 +0800

    update: update template

--- demo.py
+++ demo.py
@@ -8,11 +8,22 @@ app = web.application(urls, globals())
 
 class hello:        
     def GET(self, name):
-        fragment = tempson.createDoc('./index.tpl')
-        result = fragment.render({
-            'sayHi': 'Hello, ' + name
-        })
-        return result
+        try:
+            fragment = tempson.createDoc('./index.tpl')
+            result = fragment.render({
+                'myName': name,
+                'sayHi': 'Hello, ' + name,
+                'list': [{
+                    'name': 'JavaScript'
+                }, {
+                    'name': 'C++'
+                }, {
+                    'name': 'Python'
+                }]
+            })
+            return result
+        except Exception as e:
+            print e
 
 if __name__ == "__main__":
     app.run()
--- index.tpl
+++ index.tpl
@@ -4,8 +4,19 @@
     <title>Test tempson</title>
 </head>
 <body>
+    {% if myName == 'jason': %}
     <div>
-    	<h1>{{ sayHi }}</h1>
+        <h1>{{ sayHi }}</h1>
+    </div>
+    {% endif %}
+    <div>
+        <ul>
+            {% for item in list: %}
+                {% if not item['name'] == 'C++': %}
+                <li>{{ item['name'] }}</li>
+                {% endif %}
+            {% endfor %}
+        </ul>
     </div>
 </body>
 </html>
